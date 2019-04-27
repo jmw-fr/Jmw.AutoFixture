22d7c91d8228428e38639ec9e6e6232fcd5a46aa
Jean-Marc Weeger <jmweeger@ugc.fr>
2019-04-27 20:50:53 +0200

    InitialCommit

---
+++ test/Jmw.AutoFixtureUnitTest/Assets/MyClass.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="MyClass.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Jmw.AutoFixture.Test
+{
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Sample class under testing.
+    /// </summary>
+    public class MyClass
+    {
+        private readonly IRepository repository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyClass"/> class.
+        /// </summary>
+        /// <param name="repository">Instance of the <see cref="IRepository" />.</param>
+        public MyClass(IRepository repository)
+        {
+            this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        /// <summary>
+        /// Some function to test.
+        /// </summary>
+        /// <param name="param">Some parameter</param>
+        /// <returns>Some result.</returns>
+        public async Task<string> GetResultAsync(string param)
+        {
+            return await repository.GetSomeDataAsync(param);
+        }
+    }
+}
---
+++ test/Jmw.AutoFixtureUnitTest/MyClassUnitTest.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="MyClassUnitTest.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Jmw.AutoFixture.Test
+{
+    using global::AutoFixture;
+    using Xunit;
+
+    /// <summary>
+    /// Unit tests for <see cref="MyClass" />.
+    /// </summary>
+    public class MyClassUnitTest
+    {
+        /// <summary>
+        /// Function nominal test (i.e. normal functioning).
+        /// </summary>
+        [Fact]
+        [Trait(nameof(MyClass.GetResultAsync), nameof(MyClass))]
+        public async void GetResultAsync_Must_Nominal()
+        {
+            // Arrange
+            var moq = new MyClassMocks(TestType.Nominal);
+            var sut = moq.Fixture.Create<MyClass>();
+
+            // Act
+            var r = await sut.GetResultAsync(moq.Param);
+
+            // Assert
+            Assert.NotNull(r);
+            Assert.Equal(moq.ExpectedResult, r);
+            moq.VerifyAll(true);
+        }
+
+        /// <summary>
+        /// Test if the function correctly thow exceptions.
+        /// </summary>
+        /// <param name="testType">Type of test.</param>
+        /// <param name="expectedException">Type of expected exception.</param>
+        [Theory]
+        [Trait(nameof(MyClass.GetResultAsync), nameof(MyClass))]
+        [InlineData(TestType.ThrowException, typeof(System.InvalidOperationException))]
+        public async void GetResultAsync_Must_ThrowException(TestType testType, System.Type expectedException)
+        {
+            // Arrange
+            var moq = new MyClassMocks(testType);
+            var sut = moq.Fixture.Create<MyClass>();
+
+            // Act
+            var ex = await Assert.ThrowsAnyAsync<System.Exception>(async () => await sut.GetResultAsync(moq.Param));
+
+            // Assert
+            Assert.NotNull(ex);
+            Assert.Equal(expectedException, ex.GetType());
+        }
+    }
+}
